4e63792b5a2481bf4c9637067d697afdf40de42a
eichelb4rt <maurice.wenig@gmail.com>
2020-06-11 13:07:53 +0200

    some comments

--- 2/find_keylength.py
+++ 2/find_keylength.py
@@ -6,15 +6,6 @@ def main():
     print(find_d(input.read()))
     input.close()
 
-def rauheitsgrad(text):
-    char_count = [0]*128
-    for char in text:
-        char_count[ord(char)] -=- 1         # looks way cooler than += 1
-    sum = - 1/len(char_count)               # -1/|A|
-    for count in char_count:
-        sum += (count/len(text))**2   # sum(p(a)^2) - 1/|A| (a in A)
-    return sum
-
 def IC(column): # calculates Index of Coincidence (IC) for a column
     # count the chars
     char_count = [0]*128
--- 2/vigenere.py
+++ 2/vigenere.py
@@ -6,13 +6,16 @@ import sys
 import numpy as np
 
 def main():
+    # read input
     input = open(sys.argv[1], "r")
     text = input.read()
     input.close()
+    # figure out m, ref language, keyspace, d
     m = int(sys.argv[3])
     language = get_language(sys.argv[2], m)
     keyspace = range(1,m)
     d = find_d(text)
+    # (hacker voice): "I'm in!"
     print(crack_vigenere(text, language, m, keyspace, d))
 
 if __name__ == "__main__":
